3ef918cca83a9811e939a8bb1e502ceacbbe235d
Bruno Caú <brunoalcau@gmail.com>
2018-06-08 08:23:31 -0300

    update color current selected

--- android/src/main/java/com/zyu/ReactWheelCurvedPicker.java
+++ android/src/main/java/com/zyu/ReactWheelCurvedPicker.java
@@ -55,9 +55,9 @@ public class ReactWheelCurvedPicker extends WheelCurvedPicker {
         super.drawForeground(canvas);
 
         Paint paint = new Paint();
-        paint.setColor(Color.WHITE);
+        paint.setColor(Color.parseColor("#00CD9A"));
         int colorFrom = 0x00FFFFFF;//Color.BLACK;
-        int colorTo = Color.WHITE;
+        int colorTo = Color.parseColor("#00CD9A");
         LinearGradient linearGradientShader = new LinearGradient(rectCurItem.left, rectCurItem.top, rectCurItem.right/2, rectCurItem.top, colorFrom, colorTo, Shader.TileMode.MIRROR);
         paint.setShader(linearGradientShader);
         canvas.drawLine(rectCurItem.left, rectCurItem.top, rectCurItem.right, rectCurItem.top, paint);
